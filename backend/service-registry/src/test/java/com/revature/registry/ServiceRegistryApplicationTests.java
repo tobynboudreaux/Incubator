5ec4adeaef99659c4f42e4ec15b200ef3b45fa81
tobynboudreaux <tobynboudreaux@gmail.com>
2021-03-03 10:21:39 -0600

    added base for backend microservices architecture

---
+++ backend/service-registry/src/test/java/com/revature/registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
