d1c0eb4f6a5e03575eeb54a169f3db4645aab531
tobynboudreaux <tobynboudreaux@gmail.com>
2021-03-09 16:02:18 -0600

    added Angular base

---
+++ frontend/redact-and-chat/src/app/components/layout/heading/heading.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heading',
+  templateUrl: './heading.component.html',
+  styleUrls: ['./heading.component.css']
+})
+export class HeadingComponent implements OnInit {
+  collapsed = true;
+  collapsedDrop = true;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
